641ce210fa5dec06237d3600ffea0fcbffe846a5
root <root@localhost.localdomain>
2014-09-29 14:51:36 +0100

    Writer rewriten. Functions encapsulated

--- AP_01/writer.c
+++ AP_01/writer.c
@@ -17,77 +17,100 @@
 #define TRUE 1
 
 
+
+void writeSerial(int file_descriptor)
+{
+	int res;
+	char buf[255];
+		/*Reads line from STDIN*/
+	if(gets(buf) == NULL) 
+	{
+		printf("ERROR WITH GETS");
+		exit(-1);
+	}
+
+	int length = (int) strlen(buf) + 1;
+	buf[length - 1] = '\0';
+
+	res = write(file_descriptor,buf,length);   
+	printf("%d bytes written\n", res);
+}
+
+int openSerial (char* path, struct termios* oldtio)
+{
+	int file_descriptor;
+	struct termios newtio;
+
+	file_descriptor = open(path, O_RDWR | O_NOCTTY );
+	if (file_descriptor <0)
+	{
+		perror(path); exit(-1);
+	}
+
+	if ( tcgetattr(file_descriptor,oldtio) == -1)
+	{ /* save current port settings */
+		perror("tcgetattr");
+		exit(-1);
+	}
+
+	bzero(&newtio, sizeof(newtio));
+	newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+	newtio.c_iflag = IGNPAR;
+	newtio.c_oflag = 0;
+
+		/* set input mode (non-canonical, no echo,...) */
+	newtio.c_lflag = 0;
+
+	newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+	newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+
+		tcflush(file_descriptor, TCIOFLUSH);
+
+	if ( tcsetattr(file_descriptor,TCSANOW,&newtio) == -1) {
+		perror("tcsetattr");
+		exit(-1);
+	}
+
+	printf("New termios structure set\n");
+
+	return file_descriptor;
+
+}
+
+void closeSerial (int fd, struct termios* oldtio)
+{
+	if ( tcsetattr(fd,TCSANOW,oldtio) == -1)
+	{
+		perror("tcsetattr");
+		exit(-1);
+	}
+
+	close(fd);
+}
+
 int main(int argc, char** argv)
 {
-	int fd, res;
-	struct termios oldtio,newtio;
-	char buf[255];
+	int fd;
+	struct termios oldtio;
 
-	if ( (argc < 2) || 
-		((strcmp("/dev/ttyS1", argv[1])!=0))) {
-			printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-			exit(1);
-		}
-  /*
-    Open serial port device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
-  */
+	if ( (argc < 2) || ((strcmp("/dev/ttyS1", argv[1])!=0)))
+	{
+		printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+		exit(1);
+	}
 
 
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
-
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-    perror("tcgetattr");
-    exit(-1);
-  }
-
-  bzero(&newtio, sizeof(newtio));
-  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-  newtio.c_iflag = IGNPAR;
-  newtio.c_oflag = 0;
-
-    /* set input mode (non-canonical, no echo,...) */
-  newtio.c_lflag = 0;
-
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+	/*
+		Open serial port device for reading and writing and not as controlling tty
+		because we don't want to get killed if linenoise sends CTRL-C.
+	*/
 
 
+	fd = openSerial(argv[1], &oldtio);
 
-  /* 
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) próximo(s) caracter(es)
-  */
+	writeSerial(fd);
+		
+	closeSerial(fd, &oldtio);
 
-
-
-    tcflush(fd, TCIOFLUSH);
-
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-    	perror("tcsetattr");
-    	exit(-1);
-    }
-
-    printf("New termios structure set\n");
-
-    /*Reads line from STDIN*/
-    if(gets(buf) == NULL){printf("ERROR WITH GETS"); exit(-1); }
-
-    int length = (int) strlen(buf) + 1;
-    buf[length - 1] = '\0';
-
-    res = write(fd,buf,length);   
-    printf("%d bytes written\n", res);
-
-    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-    	perror("tcsetattr");
-    	exit(-1);
-    }
-
-
-
-
-    close(fd);
-    return 0;
-  }
+	return 0;
+}
