1998de2c198346aa1e624718249e3c751ba3526a
André Pinheiro <andrepdpinheiro@gmail.com>
2014-10-02 17:41:39 +0100

    Reader: done

--- AP_01/reader.c
+++ AP_01/reader.c
@@ -11,93 +11,129 @@
 #include <unistd.h>
 
 #define BAUDRATE B38400
+#define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
 
 
-int main(int argc, char** argv)
+
+int openSerial(char *path, struct termios* oldtio)
 {
-	int fd,c = 0, res, STOP = 0;
-	struct termios oldtio,newtio;
-	char buf[255], text[1024];
+	int fd; 
+	struct termios newtio;
 
-
-	if ( (argc < 2) || 
-		((strcmp("/dev/ttyS1", argv[1])!=0))) {
-		printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-	exit(1);
-}
-
-
-	/*
-		Open serial port device for reading and writing and not as controlling tty
-		because we don't want to get killed if linenoise sends CTRL-C.
-	*/
-
-		/* Error Testing */
-		fd = open(argv[1], O_RDWR | O_NOCTTY );
-		if (fd <0) {perror(argv[1]); exit(-1); }
-
-		/* Get old termios */
-		if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-		perror("tcgetattr");
-		exit(-1);
+	fd = open(path, O_RDWR | O_NOCTTY);
+	if(fd < 0)
+	{
+		perror(path);
+		exit(EXIT_FAILURE);
 	}
 
-		/* Reset New Termios */
+
+	if(tcgetattr(fd, newtio) == -1)
+	{
+		//Should not happen
+		perror("[!] Error on tcgetattr function.");
+		exit(EXIT_FAILURE);
+	}
+
+
 	bzero(&newtio, sizeof(newtio));
 	newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
 	newtio.c_iflag = IGNPAR;
 	newtio.c_oflag = 0;
-
-		/* set input mode (non-canonical, no echo,...) */
 	newtio.c_lflag = 0;
 
-		newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-		newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+	//Inter-character timer unused
+	newtio.c_cc[VTIME] = 0;
+	//Blocking read until 5 chars received
+	newtio.c_cc[VMIN] = 5;   
+
+
+	tcflush(fd, TCIOFLUSH);
+	if(tcsetattr(fd, TCSANOW, &newtio) == -1) 
+	{
+		//Should not happen
+		perror("[!] Error on tcsetattr function.");
+		exit(EXIT_FAILURE);
+	}
+
+
+	printf("New termios structure set\n");
+	return fd;
+}
 
 
 
-	/* 
-		VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-		leitura do(s) próximo(s) caracter(es)
-	*/
+void rewriteSerial(char *text, int fd)
+{
+	int res;
+	int length = (int) strlen(text) + 1;
+
+	buf[length - 1] = '\0';
+	res = write(fd, text, length);
+	printf("%d bytes written\n", res);
+}
 
 
 
-		tcflush(fd, TCIOFLUSH);
+void readSerial(int fd)
+{
+	char buf[255];
+	char text[1024];
+	int STOP = FALSE;
+	int currentIndex = 0, res;
 
-		if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-			perror("tcsetattr");
-			exit(-1);
+
+	while(STOP == FALSE)
+	{
+		res = read(fd, buf, 1);
+		if(res < 0) 
+		{
+			//Error
+			perror("[!] Error on read function!");
+			exit(EXIT_FAILURE);
 		}
 
-		printf("New termios structure set\n");
-
-		
-		int i = 0;
-
-	while (STOP==FALSE) {       /* loop for input */
-		res = read(fd,buf,1);   /* returns after at least 5 chars have been input */
-		text[i] = buf[0];
-		//printf("%u - %c\n", i, text[i]);
-		if (text[i++] == '\0') {
+		text[currentIndex] = buf[0];
+		if(text[currentIndex++] == '\0')
 			STOP = TRUE;
-		}
 	}
 
-	printf("\n\nCount = %u\n" , i);
-	printf("Text = %s\n" , text);
 
-
-	/* 
-		O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guião 
-	*/
+	printf("Text: %s\n", text);
+	rewriteSerial(text, fd);
+}
 
 
 
-		tcsetattr(fd,TCSANOW,&oldtio);
-		close(fd);
-		return 0;
+void closeSerial(int fd, struct termios* oldtio)
+{
+	if(tcsetattr(fd,TCSANOW,oldtio) == -1)
+	{
+		perror("[!] Error on tcsetattr function.");
+		exit(EXIT_FAILURE);
 	}
+
+	close(fd);
+}
+
+
+int main(int argc, char** argv)
+{
+	int fd,c = 0, res, STOP = 0;
+	struct termios oldtio;
+
+	if(argc < 2 || strcmp(MODEMDEVICE, argv[1]) != 0) 
+	{
+		printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+		exit(EXIT_FAILURE);
+	}
+
+
+	fd = openSerial(argv[1], &oldtio);
+	readSerial(fd);
+	closeSerial(fd, &oldtio);
+	return 0;
+}
